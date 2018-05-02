fabf3a9567397465f749048a0708360f963e9a1f
Guillermo <you@example.com>
2018-05-02 09:36:09 +0200

    Carpeta freeCodeCamp

---
+++ freeCodeCamp/ejercicio36.js
@@ -0,0 +1,12 @@
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result = "it was a " + myAdjective + " " + myNoun + " who" + " "+ myVerb + " " + myAdverb;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
