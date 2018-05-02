43ba918764cd9cb8436bc68440a5635e2ec573b0
Guillermo <you@example.com>
2018-05-02 16:22:38 +0200

    Exercices from 51 to 80

---
+++ freeCodeCamp/ejercicio76.js
@@ -0,0 +1,16 @@
+
+function isLess(a, b) {
+  // Fix this code
+  switch (true) {
+    case a < b:
+      answer = true;
+      break;
+    case a > b:
+      answer = false;
+      break;
+}
+  return answer;
+}
+
+// Change these values to test
+isLess(10, 15);
