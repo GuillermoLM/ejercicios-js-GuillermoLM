b4df87faa59b1a9b887085886d216fc68f4c4372
Guillermo <you@example.com>
2018-05-02 17:34:52 +0200

    freeCodeCamp COMPLETE

---
+++ freeCodeCamp/ejercicio88.js
@@ -0,0 +1,23 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Kaleo",
+    "title": "A/B",
+    "release_year": 2016,
+    "formats":[
+      "CS",
+      "8T",
+      "LP"]
+  }
+];
