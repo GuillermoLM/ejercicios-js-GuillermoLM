acdd49e53c1b8d344331f8ac6a5da754151a97a4
Guillermo <you@example.com>
2018-05-16 15:51:23 +0200

    if-statement corrected

--- javascripting/if-statement.js
+++ javascripting/if-statement.js
@@ -2,5 +2,5 @@ var fruit = "orange";
 if (fruit.length > 5) {
 console.log("The fruit name has more than five characters.");
 } else {
-  console.log("The fruit name has five charcaters or less."echo some text >);
+  console.log("The fruit name has five charcaters or less.");
 }
